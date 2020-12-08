1adb4241b0babc5525d359c93b16c3398cde0a22
Matthias Jäger <write@matthias-jaeger.net>
2020-12-08 22:03:07 +0100

    barrs

--- src/hatches/bars.ts
+++ src/hatches/bars.ts
@@ -5,8 +5,8 @@ export function bars(context: p5, buffer: p5, density: number, col: p5.Color) {
   gfx.image(buffer.get(), 0, 0);
   const maxDist = 100;
   const dist = maxDist * density;
-  const scl = (gfx.height / dist);
-  for (let x = 0; x < gfx.height; x += scl) {
+  const scl = (gfx.width / dist);
+  for (let x = 0; x < gfx.width; x += scl) {
     gfx.stroke(col);
     gfx.line(x, 0, x, gfx.height);
   }
